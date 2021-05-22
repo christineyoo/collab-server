91bca583241b516770e3e193eb66fec98537b53d
Christine Yoo <christinecho.s@gmail.com>
2021-05-22 14:20:06 -0700

    getById tests pass

--- test/posts-endpoints.spec.js
+++ test/posts-endpoints.spec.js
@@ -21,11 +21,11 @@ describe('Posts endpoints', function () {
   after('disconnect from db', () => db.destroy());
 
   before('clean the table', () =>
-    db.raw('TRUNCATE posts, comments RESTART IDENTITY CASCADE')
+    db.raw('TRUNCATE groups, posts, comments RESTART IDENTITY CASCADE')
   );
 
   afterEach('cleanup', () =>
-    db.raw('TRUNCATE posts, comments RESTART IDENTITY CASCADE')
+    db.raw('TRUNCATE groups, posts, comments RESTART IDENTITY CASCADE')
   );
 
   describe('GET /api/posts', () => {
@@ -52,6 +52,41 @@ describe('Posts endpoints', function () {
       });
       it('GET /api/posts responds with 200 and all the posts', () => {
         return supertest(app).get('/api/posts').expect(200, testPosts);
+      });
+    });
+  });
+
+  describe('GET /api/posts/:post_id', () => {
+    context('Given no posts', () => {
+      it('responds with 404', () => {
+        const postId = 123456;
+        return supertest(app)
+          .get(`/api/posts/${postId}`)
+          .expect(404, { error: { message: `Post doesn't exist` } });
+      });
+    });
+
+    context('Given there are posts in the database', () => {
+      const testGroups = makeGroupsArray();
+      const testPosts = makePostsArray();
+      const testComments = makeCommentsArray();
+      beforeEach('Insert posts', () => {
+        return db
+          .into('groups')
+          .insert(testGroups)
+          .then(() => {
+            return db.into('posts').insert(testPosts);
+          })
+          .then(() => {
+            return db.into('comments').insert(testComments);
+          });
+      });
+      it('responds with 200 and the specified post', () => {
+        const postId = 2;
+        const expectedPost = testPosts[postId - 1];
+        return supertest(app)
+          .get(`/api/posts/${postId}`)
+          .expect(200, expectedPost);
       });
     });
   });
