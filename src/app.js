1dd2952b6b83c327ca7f581b71ce09154d1efd1e
Christine Yoo <christinecho.s@gmail.com>
2021-05-22 17:49:48 -0700

    added postgrator-config file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const { CLIENT_ORIGIN } = require('./config');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const groupsRouter = require('./groups/groups-router');
@@ -15,7 +14,7 @@ const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(cors());
 
 app.use('/api/groups', groupsRouter);
 app.use('/api/posts', postsRouter);
